e4193bd247dcd2bc97a91896a606581ca7bb1607
--replace-all <mabrow@utu.fi>
2021-12-19 19:55:31 +0200

    day6p2-done

--- Tasks/Day6.py
+++ Tasks/Day6.py
@@ -1,39 +1,27 @@
+#Read input into "data"-list
 import collections
-import copy
 data = []
-with open("inputs/testinput6.txt","r")as f:
+with open("inputs/input6.txt","r")as f:
     for row in f:
         for value in row.strip().split(","):
             data.append(int(value))
-
-#Part1 answer was 351188
-
-fish = collections.Counter(data)
+#Function for counting the amount of spawned fish in given days
 def fishfarm(days):
-    global fish
+    fish = collections.deque()
+    for f in range(9):
+        fish.append(data.count(f))
     for day in range(days):
-        new_dict = {}
-        new_fish = fish[0] if 0 in fish.keys() else 0
-        six = fish[6] if 6 in fish.keys() else False
-        for key in fish:
-            if key == 0:
-                new_dict[6] = (new_fish + fish[7]) if 7 in fish.keys() else new_fish
-                new_dict[8] = new_fish
-            elif key == 7:
-                new_dict[6] = new_fish + fish[7]   
-            elif key == 6:
-                new_dict[5] = six
-            else:
-                new_dict[key-1] = fish[key]
-        fish = new_dict
-    sum = 0
-    for key in fish:
-        sum+=fish[key]
-    print(sum)
+        new_fish = fish.copy()[0]
+        fish.rotate(-1)
+        fish[6] += new_fish
+    return(sum(fish))  
                 
-fishfarm(80)
-fishfarm(256)
+print(f"Part1: {fishfarm(80)}")
+print(f"Part2: {fishfarm(256)}")
 
 #Output:
-#5934: is correct 
-#28671831483421 is WAY too high!
+#Part1: 351188 
+# (is correct) 
+
+#Part2: 1595779846729 
+# (is correct)
