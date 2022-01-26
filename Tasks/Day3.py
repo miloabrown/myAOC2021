f0f1e9160248128991b239850b0856d487eb0fc4
--replace-all <mabrow@utu.fi>
2022-01-26 19:43:31 +0200

    Update-solutions-for-day3

--- Tasks/Day3.py
+++ Tasks/Day3.py
@@ -1,87 +1,37 @@
-bin_list = []
-with open("inputs/input3.txt","r")as t:
-    for r in t:
-        bin_list.append(r.strip("\n"))
-        
-#PART1
+with open("inputs/sampleinput3.txt","r")as f:
+    data = [number.strip() for number in f]
 
-#Binary to decimal converter bin:str -> ans:int
-def bin_to_dec(bin):
-    ans = 0
-    p = len(bin)-1
-    for i in bin:
-        ans += int(i)*(2**p)
-        p-=1
-    return ans
-#Function to count the common bit in given index of given list. Returns "1" or "0" :str
-def count_common(bins:list,i:int):
-    sum = 0
-    for num in bins:
-        sum+= int(num[i])
-    if sum >= round(len(bins)/2):
-        return "1"
-    else:
-        return "0"
-#Function uses the count_common function to count the most common bit for each index in given list returns binary number as str
-def gamma(bins:list):
-    ans = ""
-    for x in range(len(bins[0])):
-        ans += count_common(bins,x)
-        
-    return ans
-#Converts the bits in given binary number
-def epsilon(binary):
-    ans = ""
-    temp = ""
-    for i in binary:
-        if i == "1":
-            temp = "0"
+'''Functions to find common and uncommon bits for given index'''
+def common_bit_for(binaries,index):
+    return sorted([x[index] for x in binaries])[int(len(binaries)/2)]
+
+def uncommon_bit_for(binaries,index):
+    return str(abs(int(common_bit_for(binaries,index))-1))
+
+'''PART1''' #sample input answer: 198 
+def part1():
+    epsilon=gamma=""
+    index = 0
+    while index<len(data[0]):
+        gamma+=common_bit_for(data,index)
+        epsilon+=uncommon_bit_for(data,index)  
+        index+=1
+    print("Part1:",int(gamma,2)*int(epsilon,2))
+    
+'''PART2'''
+def part2(): #sample input answer 230
+    def oxygen(binaries,index):
+        if len(binaries) > 1:
+            return oxygen(list(filter(lambda x:x[index] == common_bit_for(binaries,index),binaries)),index+1)
         else:
-            temp = "1"
-        ans+=temp
-    return ans
+            return(binaries[0])
+    def co(binaries,index):
+        if len(binaries) > 1:
+            return co(list(filter(lambda x:x[index] == uncommon_bit_for(binaries,index),binaries)),index+1)
+        else:
+            return(binaries[0])
+    print("Part2:",int(oxygen(data,0),2)*int(co(data,0),2))
 
-gamma_bin = gamma(bin_list)
-epsilon_bin = epsilon(gamma_bin)
-
-#Function that goes through binary list recursively and finds our last binary number
-
-def commons(number_list, index):
-    ans = ""
-    new_list = list(filter(lambda number: number[index] == count_common(number_list,index), number_list))
-
-    if len(new_list) == 1:
-        ans = new_list[0]
-        print(ans)
-    else:
-        print(len(new_list))
-        commons(new_list,index+1)
-    return ans
-commons(bin_list,0)
-#Test print part1
-# print(gamma_bin,epsilon_bin)
-# print(bin_to_dec(gamma_bin)*bin_to_dec(epsilon_bin))
-
-#-------------------------------------------#
-#PART2
-#Oxygen gen (commons) * CO2 scrubber(least commons)
-
-# a = bin_to_dec("011101110101")
-# b = bin_to_dec("100100000001")
-# # print(keep_commons(bin_list,0))
-# # print(keep_uncommons(bin_list,0))
-
-
-# print(f"{a} * {b} = {a*b}")
-
-#011001101000
-#111111001110
-#6635440 Too high
-
-
-#011101110101(common) 1909
-#100100000001 2305
-#4400245 Too low
-
-#525, 263, 136, 77, 41, 28, 15, 10, 6, 3, 2, 1 -> 011101110101 = 1909
-#475, 237, 118, 54, 26, 13, 5, 3, 2, 1 -> 100100000001 = 2305
+if __name__ == "__main__":
+    part1()
+    part2()
